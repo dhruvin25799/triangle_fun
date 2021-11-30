e66ceec6c18e1aec43f59273d358cc9e444bd29e
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2021-11-30 14:25:47 +0530

    Fixed area of traingle

--- scripts/area.js
+++ scripts/area.js
@@ -1,34 +1,13 @@
-const firstSide = document.querySelector("#first-side");
-const secondSide = document.querySelector("#second-side");
-const thirdSide = document.querySelector("#third-side");
-const calculate = document.querySelector("#calculate-area");
+const base = document.querySelector("#base");
+const height = document.querySelector("#height");
+const calculate = document.querySelector("#calc");
 const output = document.querySelector("#output");
 
-function calculateArea(e) {
-    e.preventDefault();
-
-    const firstSideValue = Number(firstSide.value);
-    const secondSideValue = Number(secondSide.value);
-    const thirdSideValue = Number(thirdSide.value);
-
-    if (
-        firstSideValue + secondSideValue > thirdSideValue &&
-        secondSideValue + thirdSideValue > firstSideValue &&
-        firstSideValue + thirdSideValue > secondSideValue
-    ) {
-        const semiPerimeter =
-            (firstSideValue + secondSideValue + thirdSideValue) / 2;
-
-        const result = Math.sqrt(
-            semiPerimeter *
-            (semiPerimeter - firstSideValue) *
-            (semiPerimeter - secondSideValue) *
-            (semiPerimeter - thirdSideValue)
-        ).toFixed(4);
-        output.innerText = `Using heron's formula, Area of Triangle is ${result} units`;
-    } else {
-        output.innerText = "Oh, wait! These ain't valid side lengths";
-    }
+function calculateArea() {
+    var triangleBase = Number(base.value);
+    var triangleHeight = Number(height.value);
+    var area = 0.5 * triangleBase * triangleHeight;
+    output.innerHTML = "The Area of the triangle is : " + area + "<br>Formula used is 1/2 * base * height";
 }
 
-calculate.addEventListener("submit", calculateArea);
+calculate.addEventListener("click", calculateArea);
